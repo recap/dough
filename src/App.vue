9cb200ce8e666fa23c08f0d88dea266ed2195715
Reggie Cushing <reggie@Reggies-MacBook-Pro.local>
2022-08-12 10:16:23 +0200

    using computed properties

--- src/App.vue
+++ src/App.vue
@@ -42,15 +42,15 @@
   </p>
   <p>
 	<label class="labels">Water </label>
-	<input class="w3-input w3-yellow" type="number" v-model.lazy="waterWeight" @change="reverseItem('water')" @paste="reverseItem('water')" @keyup="reverseItem('water')">
+	<input class="w3-input w3-yellow" type="number" v-model.lazy="waterWeight">
   </p>
   <p>
 	<label class="labels">Starter  </label>
-	<input class="w3-input w3-yellow" type="number" v-model.lazy="cultureWeight" @change="reverseItem('culture')" @paste="reverseItem('culture')" @keyup="reverseItem('culture')">
+	<input class="w3-input w3-yellow" type="number" v-model.lazy="cultureWeight">
   </p>
   <p>
 	<label class="labels">Salt </label>
-	<input class="w3-input w3-yellow" type="number" v-model.lazy="saltWeight" @change="reverseItem('salt')" @paste="reverseItem('salt')" @keyup="reverseItem('salt')">
+	<input class="w3-input w3-yellow" type="number" v-model.lazy="saltWeight">
   </p>
   <p>
 	<label class="labels">Flour composition</label>
@@ -58,15 +58,15 @@
   </p>
   <p>
 	<label class="labels">Egg </label>
-	<input class="w3-input w3-yellow" type="number" v-model.lazy="eggWeight" @change="reverseItem('egg')" @paste="reverseItem('egg')" @keyup="reverseItem('egg')">
+	<input class="w3-input w3-yellow" type="number" v-model.lazy="eggWeight"> 
   </p>
   <p>
 	<label class="labels">Butter </label>
-	<input class="w3-input w3-yellow" type="number" v-model.lazy="butterWeight" @change="reverseItem('butter')" @paste="reverseItem('butter')" @keyup="reverseItem('butter')">
+	<input class="w3-input w3-yellow" type="number" v-model.lazy="butterWeight">
   </p>
   <p>
 	<label class="labels">Sugar </label>
-	<input class="w3-input w3-yellow" type="number" v-model.lazy="sugarWeight" @change="reverseItem('sugar')" @paste="reverseItem('sugar')" @keyup="reverseItem('sugar')">
+	<input class="w3-input w3-yellow" type="number" v-model.lazy="sugarWeight"> 
   </p>
 </div>
 </div>
@@ -79,7 +79,7 @@
 </style>
 
 <script setup>
-    import { ref, onMounted } from 'vue'
+    import { ref, onMounted, computed } from 'vue'
     const finalDoughWeight = ref(1200)
 
     const waterPercentage = ref(75)
@@ -91,73 +91,36 @@
     const flourComposition = ref('40:30:30')
 
     const flourWeight = ref(0)
-    const waterWeight = ref(0)
-    const saltWeight = ref(0)
-    const eggWeight = ref(0)
-    const butterWeight = ref(0)
-    const sugarWeight = ref(0)
-    const cultureWeight = ref(0)
+    const computedTemplate = (refObject) => {
+      return {
+        get() {
+          return Math.round(refObject.value / 100 * flourWeight.value)
+        },
+        set(newValue) {
+          flourWeight.value = (newValue / refObject.value) * 100
+          reverseCalculate()
+        }
+      }
+    }
+    const waterWeight = computed(computedTemplate(waterPercentage))
+    const saltWeight = computed(computedTemplate(saltPercentage))
+    const eggWeight = computed(computedTemplate(eggPercentage))
+    const butterWeight = computed(computedTemplate(butterPercentage))
+    const sugarWeight = computed(computedTemplate(sugarPercentage))
+    const cultureWeight = computed(computedTemplate(culturePercentage))
     const flourCompositionWeight = ref('')
 
-    const registry = {
-      sugar: {
-        percentage: sugarPercentage,
-        weight: sugarWeight
-      },
-      culture: {
-        percentage: culturePercentage,
-        weight: cultureWeight
-      },
-      salt: {
-        percentage: saltPercentage,
-        weight: saltWeight
-      },
-      water: {
-        percentage: waterPercentage,
-        weight: waterWeight
-      },
-      egg: {
-        percentage: eggPercentage,
-        weight: eggWeight
-      },
-      butter: {
-        percentage: butterPercentage,
-        weight: butterWeight
-      }
-    }
-
-
-    function reverseItem(itemName) {
-      const item = registry[itemName]
-      if(!item) {
-        return
-      }
-      flourWeight.value = (item.weight.value / item.percentage.value) * 100
-      console.log("rev water: ", flourWeight.value)
-      reverseCalculate()
-    }
 
     function reverseCalculate() {
       const p = (v) => v.value / 100
       finalDoughWeight.value = Math.round(flourWeight.value * (p(waterPercentage) + p(eggPercentage) + p(culturePercentage) + p(butterPercentage) + p(saltPercentage) + p(sugarPercentage)) + flourWeight.value)
-
       calculate()
-
       console.log(`final dough weight: ${finalDoughWeight.value}`)
     }
 
     function calculate() {
       flourWeight.value = Math.round((finalDoughWeight.value * 100) / (waterPercentage.value + culturePercentage.value + saltPercentage.value + eggPercentage.value + butterPercentage.value + sugarPercentage.value + 100))
-      const convertToWeight = (refItem) => Math.round(refItem.value / 100 * flourWeight.value)
-      waterWeight.value = convertToWeight(waterPercentage)
-      cultureWeight.value = convertToWeight(culturePercentage)
-      saltWeight.value = convertToWeight(saltPercentage)
-      eggWeight.value = convertToWeight(eggPercentage)
-      sugarWeight.value = convertToWeight(sugarPercentage)
-      butterWeight.value = convertToWeight(butterPercentage)
-
       const sum = flourComposition.value.split(':').reduce((a,b) => parseInt(a) + parseInt(b), 0)
-
       if(sum != 100) {
         flourCompositionWeight.value = "WRONG RATIOS!"
       }else {
